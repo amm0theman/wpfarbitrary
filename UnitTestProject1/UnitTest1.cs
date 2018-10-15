591691c71618ecccc3bedbfdc17206c8b7a11afc
amm0theman <ammonriley.97@gmail.com>
2018-10-15 09:56:54 -0600

    first commit

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WpfApp3;
+using NUnit;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var vm = new EntryFieldVM();
+            Assert.AreEqual("Username", vm.Username);
+            Assert.AreEqual("Password", vm.Password);
+            Assert.AreEqual("Email", vm.Email);
+        }
+    }
+}
---
+++ WpfApp3/Converter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace WpfApp3
+{
+    class Converter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (targetType != typeof(Brush))
+                throw new Exception();
+            int num = int.Parse(value.ToString());
+
+            return (num == 0 ? Brushes.PeachPuff : Brushes.Transparent);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
